e41069540424cc341ad4657750b4de9a6611d0e8
Evan <evan@DESKTOP-VFQ0KLU>
2018-02-28 21:25:09 -0500

    Homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Jake's Large Clitoris
                 </div>
 
                 <div class="links">
